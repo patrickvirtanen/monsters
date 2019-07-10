14cdb77361af48296e98778616813e814b1d680e
Patrick Virtanen <patrick.virtanen@gmail.com>
2019-07-10 19:18:43 +0200

    Updates

---
+++ precache-manifest.ec545e611cf917be84876f004d74fc04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a72cf46346a2d1d6a2dd463010ead5d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8e5ef711bbf9a240ab98",
+    "url": "/monsters/static/css/main.6a518e92.chunk.css"
+  },
+  {
+    "revision": "62c1ada391dd31af0cab",
+    "url": "/monsters/static/js/2.a59449a3.chunk.js"
+  },
+  {
+    "revision": "8e5ef711bbf9a240ab98",
+    "url": "/monsters/static/js/main.87b56680.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
